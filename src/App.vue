d0b2d0bb8349b45b56f46c2b3b18bd0e57ff3518
Frank Tröger <frank.troeger@fau.de>
2019-03-02 14:43:32 +0100

    starting new Iptables component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <Iptables msg="Test"/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Iptables from "./components/Iptables";
 
 export default {
   name: 'app',
   components: {
+    Iptables,
     HelloWorld
   }
 }
---
+++ src/components/Iptables.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button class="btn btn-primary" v-on:click="updateRules">Rulez</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Iptables',
+  props: {
+    msg: String
+  },
+  data: function() {
+    return {
+      tables: {},
+    }
+  },
+  methods: {
+    updateRules: function() {
+      var vm = this;
+      $.ajax({
+        url: 'dummy.xml',
+        success: function(data) {
+          alert(data);
+          vm.tables = data;
+        },
+      });
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
