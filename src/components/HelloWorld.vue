3a4ba27d49431ad10aeefcc3ac948a281eabe710
Frank Tröger <frank.troeger@fau.de>
2019-03-02 13:30:54 +0100

    add bootstrap

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+      With Bootstrap!
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
