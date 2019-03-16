fe8112bbc85f7fda84cc7a48de45ebd475671351
Frank Tröger <frank.troeger@fau.de>
2019-03-16 16:49:01 +0100

    configurable via iptables2web.json

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <img src="./assets/logo.png" style="position: fixed; top:-16px; right:-8px; z-index: 1050;" width="96px;" title="iptables2web"/>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-      <a class="navbar-brand" href="javascript:void(0)">iptables2web</a>
+      <a class="navbar-brand" href="javascript:void(0)">iptables2web v{{version}} <i class="fas fa-circle" v-bind:class="{ 'text-success': autoUpdate, 'text-danger': !autoUpdate }"></i></a>
+
       <button class="navbar-toggler" style="margin-right: 80px;" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -13,13 +14,13 @@
             <a class="nav-link" v-bind:href="homeUrl">Home <span class="sr-only">(current)</span></a>
           </li>
         </ul>
-        <div class="form-inline my-2 my-lg-0" style="margin-right: 80px;" >
-          <input class="form-control mr-sm-2" type="text" placeholder="URL to cgi-bin" aria-label="URL to cgi-bin" v-model="dataUrl" v-bind:disabled="autoUpdate">
-          <button class="btn btn-outline-success my-2 my-sm-0" v-on:click="updateRules" title="Load"><i class="fas fa-cloud-download-alt"></i></button>
+        <div class="form-inline my-2 my-lg-0" style="margin-right: 80px;">
+          <input class="form-control mr-sm-2" type="text" style="width: 10em;" placeholder="URL to cgi-bin" aria-label="URL to cgi-bin" v-model="dataUrl" v-bind:disabled="autoUpdate">
+          <button class="btn btn-outline-success my-2 my-sm-0 ml-3" v-on:click="updateRules" title="Load"><i class="fas fa-cloud-download-alt"></i></button>
           <button class="btn btn-outline-success my-2 my-sm-0 ml-3" v-on:click="play" v-if="!autoUpdate" title="Start"><i class="fas fa-play"></i></button>
           <button class="btn btn-outline-success my-2 my-sm-0 ml-3" v-on:click="pause" v-if="autoUpdate" title="Pause"><i class="fas fa-pause"></i></button>
-          <button class="btn btn-outline-success my-2 my-sm-0 ml-3" v-on:click="login" title="Login"><i class="fas fa-sign-in-alt"></i></button>
-          <button class="btn btn-outline-success my-2 my-sm-0 ml-3" v-on:click="logout" title="Logout"><i class="fas fa-sign-out-alt"></i></button>
+          <button v-if="enableLogin" class="btn btn-outline-success my-2 my-sm-0 ml-3" v-on:click="login" title="Login"><i class="fas fa-sign-in-alt"></i></button>
+          <button v-if="enableLogin" class="btn btn-outline-success my-2 my-sm-0 ml-3" v-on:click="logout" title="Logout"><i class="fas fa-sign-out-alt"></i></button>
         </div>
       </div>
 
@@ -32,14 +33,39 @@
 <script>
 import Iptables from "./components/Iptables";
 
+let localData = {
+  homeUrl: process.env.BASE_URL,
+  autoUpdate : false,
+  dataUrl: '/cgi-bin/iptables2xml',
+  enableLogin: false,
+  loginUrl: "/mellon/login?ReturnTo=RETURN_TO_URL",
+  logoutUrl: "/mellon/logout?ReturnTo=RETURN_TO_URL",
+  version: null,
+};
+
+fetch('./iptables2web.json')
+.then(
+  function(response) {
+    if (response.status !== 200) {
+      console.log('Looks like there was a problem. Status Code: ' +  response.status);
+      return;
+    }
+
+    response.json().then(function(data) {
+      Object.keys(data).forEach(function(key) {
+        localData[key] = data[key];
+      });
+    });
+  }
+)
+.catch(function(err) {
+  console.log('Fetch Error :-S', err);
+});
+
 export default {
   name: 'app',
   data: function() {
-    return {
-      dataUrl: '/cgi-bin/iptables2xml',
-      homeUrl: process.env.BASE_URL,
-      autoUpdate : false,
-    }
+    return localData
   },
   components: {
     Iptables,
@@ -56,10 +82,10 @@ export default {
       this.$refs.iptables.updateRules();
     },
     login: function() {
-      window.location.href='/mellon/login?ReturnTo=' + window.location.href;
+      window.location.href = this.loginUrl.replace(/RETURN_TO_URL/, window.location.href);
     },
     logout: function() {
-      window.location.href='/mellon/logout?ReturnTo=' + window.location.href;
+      window.location.href = this.logoutUrl.replace(/RETURN_TO_URL/, window.location.href);
     },
     play: function() {
       this.autoUpdate = true;
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,30 @@
 // vue.config.js
+let myPackage = require('./package.json');
+const CopyPlugin = require('copy-webpack-plugin')
+
+function modify(buffer) {
+    let myJson = JSON.parse(buffer.toString());
+
+    // make any modifications you like, such as
+    myJson.version = myPackage.version;
+
+    return JSON.stringify(myJson, null, 2);
+}
+
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? '/iptables2web/' : '/',
     crossorigin: 'use-credentials',
+
+    configureWebpack: {
+        plugins: [
+            new CopyPlugin([
+                {
+                    from: "./src/iptables2web.json",
+                    to:   "./iptables2web.json",
+                    transform (content, path) {
+                        return modify(content)
+                    }
+                }])
+        ]
+    }
 }
